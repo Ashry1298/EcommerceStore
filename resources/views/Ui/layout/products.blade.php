2ac550b353e85169e4d6b37407f2d5cdfb1eac38
Mostafa Alashry <mostafaelashry36@gmail.com>
2023-06-13 19:08:28 +0300

    Adding Product Component

--- resources/views/Ui/layout/products.blade.php
+++ resources/views/Ui/layout/products.blade.php
@@ -5,7 +5,6 @@
                 Product Overview
             </h3>
         </div>
-
         <div class="flex-w flex-sb-m p-b-52">
             <div class="flex-w flex-l-m filter-tope-group m-tb-10"> 
                 <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
@@ -244,40 +243,7 @@
 
         <div class="row isotope-grid">
             @foreach ($products as $product) 
-            <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
-                <!-- Block2 -->
-                <div class="block2">
-                 
-                    <div class="block2-pic hov-img0 label-new" data-label="New">
-                        <img src="{{asset('uploads/products/'.$product->main_image)}}" alt="IMG-PRODUCT">
-                        <a href="#"
-                            class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-                            Quick View
-                        </a>
-                    </div>
-
-                    <div class="block2-txt flex-w flex-t p-t-14">
-                        <div class="block2-txt-child1 flex-col-l ">
-                            <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                               {{$product->name_en}}-{{$product->name_ar}}
-                            </a>
-
-                            <span class="stext-105 cl3">
-                                <b>${{$product->price}}</b>
-                            </span>
-                        </div>
-
-                        <div class="block2-txt-child2 flex-r p-t-3">
-                            <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                <img class="icon-heart1 dis-block trans-04"
-                                    src="Ui/images/icons/icon-heart-01.png" alt="ICON">
-                                <img class="icon-heart2 dis-block trans-04 ab-t-l"
-                                    src="Ui/images/icons/icon-heart-02.png" alt="ICON">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-product-card :product="$product" />
                 
             @endforeach
 
